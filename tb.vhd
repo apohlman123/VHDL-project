5cb77e6b9d2436264831051a3590be9976eb42a0
Joe Gorman <jgorman3@lion.lmu.edu>
2019-03-24 17:38:51 -0700

    added assertions syntax
    this will be needed in the future.

--- tb.vhd
+++ tb.vhd
@@ -26,6 +26,10 @@ use ieee.std_logic_1164.all;
 
 --open and close file
 --architecture test of my_testbench
-  --filetest_data_file: text;
+--  filetest_data_file: text;
 --begin
-  --file_open(test_data_file, “C:\test.txt”, read_mode);
+--  file_open(test_data_file, “C:\test.txt”, read_mode);
+
+
+--assertions will be made to verify transmitted & received data
+--assert(transmitted_data) report "It's correct!"

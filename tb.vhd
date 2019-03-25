cd2ad2de0338bf97c46cdf0805fb8908633a0640
Joe Gorman <jgorman3@lion.lmu.edu>
2019-03-24 17:08:17 -0700

    created tb.vhd
    file serves to establish header info and stub test bench (file will be renamed)

---
+++ tb.vhd
@@ -0,0 +1,22 @@
+------------------------------------------------------------------------------
+--Company:      Loyola Marymount University
+--Engineer:     Joseph Gorman
+
+--Create Date:  03/24/2019
+--Design Name:
+--Module Name:
+--Project Name: PCM Encoding/Decoding
+--Tool Versions:
+--Description:
+
+--Dependencies:
+
+--Revisions:
+------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+--entity
+
+--architecture

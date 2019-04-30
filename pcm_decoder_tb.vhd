0c277acd488cf73b777d734298fdff17b482a502
claysauter <47647451+claysauter@users.noreply.github.com>
2019-04-29 20:59:42 -0700

    Testbench after adding LRCK to RTL.

--- pcm_decoder_tb.vhd
+++ pcm_decoder_tb.vhd
@@ -26,61 +26,81 @@ entity pcm_decoder_tb is
 end entity;
 
 architecture test of pcm_decoder_tb is
-    signal clk_i_s     : std_logic;
-    signal decoder_d_i_s : std_logic;
-    signal rst_i_async_s : std_logic;
-    signal decoder_q_o_s : std_logic_vector(7 downto 0);
-    
-    constant clock_period : time := 10 ns;
-    
+    signal b_clk_i_s       : std_logic;
+    signal decoder_d_i_s   : std_logic;
+    signal rst_i_async_s   : std_logic;
+   -- signal decoder_q_o_s   : std_logic_vector(7 downto 0);
+	signal LRCK_i_s        : std_logic;
+    signal L_decoder_q_o_s : std_logic_vector(7 downto 0);
+	signal R_decoder_q_o_s : std_logic_vector(7 downto 0);
+	
+	constant clock_period : time := 35 ns;
+	constant frame_clock_period : time := 560 ns;
     begin
         uut: entity work.pcm_decoder(behav)
         generic map(
             bit_depth => 8
         )
         port map(
-            clk_i => clk_i_s,
-            decoder_d_i => decoder_d_i_s,
-            decoder_q_o => decoder_q_o_s,
-            rst_i_async => rst_i_async_s
+            b_clk_i => b_clk_i_s,
+            decoder_d_i   => decoder_d_i_s,
+            rst_i_async   => rst_i_async_s,
+			LRCK_i        => LRCK_i_s,
+			L_decoder_q_o => L_decoder_q_o_s,
+			R_decoder_q_o => R_decoder_q_o_s
         );
         
         clock_process : process
         begin
-            clk_i_s <= '0';
+            b_clk_i_s <= '0';
             wait for clock_period/2;
-            clk_i_s <= '1';
+            b_clk_i_s <= '1';
             wait for clock_period/2;
         end process;
-        
+ 
+        frame_clock_process : process
+        begin
+            LRCK_i_s <= '0';
+            wait for frame_clock_period/2;
+            LRCK_i_s <= '1';
+            wait for frame_clock_period/2;
+        end process;
+                
         stim_process : process
         begin
-            rst_i_async_s <= '1';
-            decoder_d_i_s <= '0';
-            wait for 10 ns;
-            rst_i_async_s <= '0';
-            decoder_d_i_s <= '0';
-            wait for 10 ns;
-            rst_i_async_s <= '0';
-            decoder_d_i_s <= '1';
-            wait for 10 ns;
-            rst_i_async_s <= '0';
-            decoder_d_i_s <= '0';
-            wait for 10 ns;
-            rst_i_async_s <= '0';
-            decoder_d_i_s <= '1';
-            wait for 10 ns;
-            rst_i_async_s <= '0';
-            decoder_d_i_s <= '1';
-            wait for 10 ns;
-            rst_i_async_s <= '0';
-            decoder_d_i_s <= '1';
-            wait for 10 ns;
-            rst_i_async_s <= '0';
-            decoder_d_i_s <= '0';
-            wait for 10 ns;
-            rst_i_async_s <= '0';
-            decoder_d_i_s <= '0';
-            wait;
+        
+        decoder_d_i_s <= '0';
+        rst_i_async_s <= '1';
+        wait for clock_period;
+        rst_i_async_s <= '0';
+        wait for clock_period/2;
+        rst_i_async_s <= '0';
+        decoder_d_i_s <= '0';
+        wait for clock_period;
+        rst_i_async_s <= '0';
+        decoder_d_i_s <= '0';
+        wait for clock_period;
+        rst_i_async_s <= '0';
+        decoder_d_i_s <= '1';
+        wait for clock_period;
+        rst_i_async_s <= '0';
+        decoder_d_i_s <= '0';
+        wait for clock_period;
+        rst_i_async_s <= '0';
+        decoder_d_i_s <= '1';
+        wait for clock_period;
+        rst_i_async_s <= '0';
+        decoder_d_i_s <= '1';
+        wait for clock_period;
+        rst_i_async_s <= '0';
+        decoder_d_i_s <= '1';
+        wait for clock_period;
+        rst_i_async_s <= '0';
+        decoder_d_i_s <= '0';
+        wait for clock_period;
+        rst_i_async_s <= '0';
+        decoder_d_i_s <= '0';
+        wait;
+        
         end process;
 end test;

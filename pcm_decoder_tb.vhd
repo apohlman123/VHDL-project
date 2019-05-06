9dcbbd5c457123a3ebc61432a585bb682d4bfbb4
claysauter <47647451+claysauter@users.noreply.github.com>
2019-05-06 01:11:31 -0700

    Updated tb with clock enables

--- pcm_decoder_tb.vhd
+++ pcm_decoder_tb.vhd
@@ -26,44 +26,35 @@ entity pcm_decoder_tb is
 end entity;
 
 architecture test of pcm_decoder_tb is
-    signal b_clk_i_s       : std_logic;
+    signal MCLK_i_s        : std_logic;
     signal decoder_d_i_s   : std_logic;
     signal rst_i_async_s   : std_logic;
-   -- signal decoder_q_o_s   : std_logic_vector(7 downto 0);
-	signal LRCK_i_s        : std_logic;
     signal L_decoder_q_o_s : std_logic_vector(7 downto 0);
 	signal R_decoder_q_o_s : std_logic_vector(7 downto 0);
 	
-	constant clock_period : time := 35 ns;
-	constant frame_clock_period : time := 560 ns;
+	constant clock_period : time := 1 ns;
+	
     begin
         uut: entity work.pcm_decoder(behav)
         generic map(
-            bit_depth => 8
+             bit_depth      => 8,                                                 --Variable bit depth (8,16,24,32)
+             sample_freq    => 44100,                                             --Variable sampling frequency (44.1k,48k,96k,192k)
+             MCLK_freq      => 28224000
         )
         port map(
-            b_clk_i => b_clk_i_s,
+            MCLK_i => MCLK_i_s,
             decoder_d_i   => decoder_d_i_s,
             rst_i_async   => rst_i_async_s,
-			LRCK_i        => LRCK_i_s,
 			L_decoder_q_o => L_decoder_q_o_s,
 			R_decoder_q_o => R_decoder_q_o_s
         );
         
-        clock_process : process
+        bit_clock_process : process
         begin
-            b_clk_i_s <= '0';
+            MCLK_i_s <= '0';
             wait for clock_period/2;
-            b_clk_i_s <= '1';
+            MCLK_i_s <= '1';
             wait for clock_period/2;
-        end process;
- 
-        frame_clock_process : process
-        begin
-            LRCK_i_s <= '0';
-            wait for frame_clock_period/2;
-            LRCK_i_s <= '1';
-            wait for frame_clock_period/2;
         end process;
                 
         stim_process : process
@@ -71,33 +62,78 @@ architecture test of pcm_decoder_tb is
         
         decoder_d_i_s <= '0';
         rst_i_async_s <= '1';
-        wait for clock_period;
+        wait for (40*clock_period)/2;
         rst_i_async_s <= '0';
-        wait for clock_period/2;
+        --wait for clock_period/2;
+        --rst_i_async_s <= '0';
+        decoder_d_i_s <= '0';
+        wait for (40*clock_period);
         rst_i_async_s <= '0';
         decoder_d_i_s <= '0';
-        wait for clock_period;
+        wait for (40*clock_period);
+        rst_i_async_s <= '0';
+        decoder_d_i_s <= '1';
+        wait for (40*clock_period);
         rst_i_async_s <= '0';
         decoder_d_i_s <= '0';
-        wait for clock_period;
+        wait for (40*clock_period);
         rst_i_async_s <= '0';
         decoder_d_i_s <= '1';
-        wait for clock_period;
+        wait for (40*clock_period);
+        rst_i_async_s <= '0';
+        decoder_d_i_s <= '1';
+        wait for (40*clock_period);
+        rst_i_async_s <= '0';
+        decoder_d_i_s <= '1';
+        wait for (40*clock_period);
         rst_i_async_s <= '0';
         decoder_d_i_s <= '0';
-        wait for clock_period;
+        wait for (40*clock_period);
         rst_i_async_s <= '0';
         decoder_d_i_s <= '1';
-        wait for clock_period;
+        wait for (40*clock_period);
         rst_i_async_s <= '0';
         decoder_d_i_s <= '1';
-        wait for clock_period;
+        wait for (40*clock_period);
         rst_i_async_s <= '0';
         decoder_d_i_s <= '1';
-        wait for clock_period;
+        wait for (40*clock_period);
+        rst_i_async_s <= '0';
+        decoder_d_i_s <= '1';
+        wait for (40*clock_period);
         rst_i_async_s <= '0';
         decoder_d_i_s <= '0';
-        wait for clock_period;
+        wait for (40*clock_period);
+        rst_i_async_s <= '0';
+        decoder_d_i_s <= '0';
+        wait for (40*clock_period);
+        rst_i_async_s <= '0';
+        decoder_d_i_s <= '0';
+        wait for (40*clock_period);
+        rst_i_async_s <= '0';
+        decoder_d_i_s <= '0';
+        wait for (40*clock_period);
+        rst_i_async_s <= '0';
+        decoder_d_i_s <= '1';
+        wait for (40*clock_period);
+        rst_i_async_s <= '0';
+        decoder_d_i_s <= '0';
+        wait for (40*clock_period);
+        rst_i_async_s <= '0';
+        decoder_d_i_s <= '1';
+        wait for (40*clock_period);
+        rst_i_async_s <= '0';
+        decoder_d_i_s <= '0';
+        wait for (40*clock_period);
+        rst_i_async_s <= '0';
+        decoder_d_i_s <= '1';
+        wait for (40*clock_period);
+        rst_i_async_s <= '0';
+        decoder_d_i_s <= '0';
+        wait for (40*clock_period);
+        rst_i_async_s <= '0';
+        decoder_d_i_s <= '1';
+        wait for (40*clock_period);
         rst_i_async_s <= '0';
         decoder_d_i_s <= '0';
         wait;

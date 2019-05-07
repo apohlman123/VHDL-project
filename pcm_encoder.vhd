b26e108d448cb8882506a675cb3a20ce12103c74
Austin Pohlman <apohlman@lion.lmu.edu>
2019-05-06 17:13:22 -0700

    Deleted commented-out code, added explanatory comments

--- pcm_encoder.vhd
+++ pcm_encoder.vhd
@@ -14,13 +14,10 @@
 
 
 --Revisions:
-    --A : created file, wrote encoder_process
-    --B : reversed operation to Big Endian (MSB first)
+    --A (03/25/19) : created file, wrote encoder_process
+    --B (04/01/19) : reversed operation to Big Endian (MSB first)
+    --See github for full revision history
 ------------------------------------------------------------------------------
---Determine left or right channel mux based on the level of LRCK_i, but OR together the...
---...edge-detection logic and feed into parallel shift in mux to shift data only on edge
-
-
 LIBRARY IEEE;
 USE IEEE.std_logic_1164.ALL;
 
@@ -28,14 +25,12 @@ ENTITY pcm_encoder IS
     GENERIC (
         bit_depth   : integer := 8;                                --Variable bit depth (8,16,24,32)
         sample_freq : integer := 44100;                            --Variable sampling frequency (44.1k,48k,96k,192k)
-        MCLK_freq   : integer := 28224000
-    );
-
-    PORT ( --Delete b_clk_i, Delete LRCK_i, they are internal signals now
-        --b_clk_i       : IN std_logic;                              --Bit Clock input to shift data
-        L_encoder_d_i : IN std_logic_vector(bit_depth-1 downto 0); --Parallel data input
-        R_encoder_d_i : IN std_logic_vector(bit_depth-1 downto 0);
-        --LRCK_i        : IN std_logic;                              --Frame clock input
+        MCLK_freq   : integer := 28224000                          --Note that for 48k,96k,192k, MCLK must equal 24576000
+    );                                                             --This is because 28224000 is not evenly divisible for
+                                                                   --all varying sample freqs
+    PORT (
+        L_encoder_d_i : IN std_logic_vector(bit_depth-1 downto 0); --Parallel data input left
+        R_encoder_d_i : IN std_logic_vector(bit_depth-1 downto 0); --Parallel data input right
         MCLK_i        : IN std_logic;                              --Master clock input
         rst_i_async   : IN std_logic;                              --Asynchronous reset for ALL DFFs
         encoder_q_o   : OUT std_logic                              --Serial data output
@@ -45,18 +40,18 @@ END pcm_encoder;
 ARCHITECTURE behav OF pcm_encoder IS
     signal q_sig          : std_logic_vector(bit_depth-1 downto 1);   --Internal DFF outputs
     signal encoder_d_sig  : std_logic_vector(bit_depth-1 downto 0);   --LR Mux Output
-    signal edge_LRCK_sig  : std_logic;
-    signal dff_q_LREdge   : std_logic;
+    signal edge_LRCK_sig  : std_logic;                                --LRCK edge indicator
+    signal dff_q_LREdge   : std_logic;                                --Internal DFF for edge indication
     constant gnd_sig      : std_logic_vector(bit_depth-1 downto 1) := (others => '0');  --Used for GND in reset
 
-    constant BCK_freq     : integer := sample_freq*bit_depth*2;
-    constant BCK_count    : integer := MCLK_freq/(2*BCK_freq);    --For bit depth 8, fs 44.1kHz, this is 20
-    signal clk_counter    : integer range 0 to MCLK_freq/(2*sample_freq); --For bit depth 8, fs 44.1kHz, this is 640
+    constant BCK_freq     : integer := sample_freq*bit_depth*2;       --Calculate frequency of bit clock
+    constant BCK_count    : integer := MCLK_freq/(2*BCK_freq);        --For bit depth 8, fs 44.1kHz, this is 20
+    signal clk_counter    : integer range 0 to MCLK_freq/(2*sample_freq); --For bit depth 8, fs 44.1kHz, this is 320
     signal clk_diff       : integer range 0 to MCLK_freq/(2*sample_freq); --Keep track of difference between edges
     signal BCK_i   : std_logic; --Bit Clock
     signal LRCK_i  : std_logic; --Frame Clock
 BEGIN
-    sync_clk_en : PROCESS(rst_i_async, MCLK_i)
+    sync_clk_en : PROCESS(rst_i_async, MCLK_i) --Synchronous Clock Enable for LRCK_i and BCK_i
     BEGIN
         IF rst_i_async = '1' THEN
             clk_counter <= 0;
@@ -64,29 +59,25 @@ BEGIN
             LRCK_i <= '0';
             BCK_i <= '0';
         ELSIF rising_edge(MCLK_i) THEN
-            IF clk_counter = MCLK_freq/(2*sample_freq) THEN
+            IF clk_counter = MCLK_freq/(2*sample_freq) THEN --LRCK_i enable at max clk_counter
                 clk_diff <= 0;
-                clk_counter <= 1;
-                BCK_i <= NOT(BCK_i);
+                clk_counter <= 1;     -- =1 to avoid overcounting on transitions
+                BCK_i <= NOT(BCK_i);  --Flip state instead of set to 1 for 50% duty cycle
                 LRCK_i <= NOT(LRCK_i);
-            ELSIF clk_counter - clk_diff = BCK_count THEN
-                clk_diff <= clk_counter;
+            ELSIF clk_counter - clk_diff = BCK_count THEN --intermediate BCK_i enable every BCK_count
+                clk_diff <= clk_counter; --Keep track of clock counts since last BCK_i enable
                 BCK_i <= NOT(BCK_i);
                 clk_counter <= clk_counter + 1;
             ELSE
                 clk_counter <= clk_counter + 1;
-            --ELSIF clk_counter - clk_diff <= BCK_count/2 THEN
-            --    clk_counter <= clk_counter + 1;
-            --    BCK_i <= '0';
-            --    LRCK_i <= '0';
             END IF;
         END IF;
     END PROCESS sync_clk_en;
 
-    encoder_d_sig <= R_encoder_d_i WHEN LRCK_i = '1' ELSE L_encoder_d_i;
-    edge_LRCK_sig <= (NOT(dff_q_LREdge) AND LRCK_i) OR (dff_q_LREdge AND NOT(LRCK_i));
+    encoder_d_sig <= R_encoder_d_i WHEN LRCK_i = '1' ELSE L_encoder_d_i;               --Input mux chooses L or R data
+    edge_LRCK_sig <= (NOT(dff_q_LREdge) AND LRCK_i) OR (dff_q_LREdge AND NOT(LRCK_i)); --Edge detection for LRCK_i
 
-    clk_edge_process : PROCESS(rst_i_async, BCK_i)
+    clk_edge_process : PROCESS(rst_i_async, BCK_i) --RTL for edge detection circuit
     BEGIN
         IF rst_i_async = '1' THEN
             dff_q_LREdge <= '0';
@@ -95,27 +86,27 @@ BEGIN
         END IF;
     END PROCESS clk_edge_process;
 
-    encoder_process : PROCESS(rst_i_async, BCK_i)
+    encoder_process : PROCESS(rst_i_async, BCK_i) --Main encoder process
     BEGIN
         IF rst_i_async = '1' THEN
             q_sig <= gnd_sig;
             encoder_q_o <= '0';
-        ELSIF rising_edge(BCK_i) THEN                          --Infer DFFs and Muxes
-            IF edge_LRCK_sig = '1' THEN                      --Data input should occur at the start of each N-bit frame
-                                                                 --mux is tied to LRCK to shift parallel data in
+        ELSIF rising_edge(BCK_i) THEN                         --Infer DFFs and Muxes
+            IF edge_LRCK_sig = '1' THEN                       --Data input should occur at the start of each N-bit frame
+                                                              --mux is tied to edge of LRCK to shift parallel data in
                 FOR i in bit_depth-1 downto 1 LOOP
-                    q_sig(i) <= encoder_d_sig(bit_depth-1-i);      --Assign DFF outputs w/ parallel data LSB->MSB
+                    q_sig(i) <= encoder_d_sig(bit_depth-1-i); --Assign DFF outputs w/ parallel data LSB->MSB
                 END LOOP;
-                encoder_q_o <= encoder_d_sig(bit_depth-1);         --Assign encoder output w/ MSB of parallel data
-            ELSE                                                --Conditional to shift serial data out
+                encoder_q_o <= encoder_d_sig(bit_depth-1);    --Assign encoder output w/ MSB of parallel data
+            ELSE                                              --Shift serial out when not edge of LRCK
                 FOR i in bit_depth-2 downto 1 LOOP
-                    q_sig(i) <= q_sig(i+1);                      --Connect inferred DFFs LSB-to-MSB
+                    q_sig(i) <= q_sig(i+1);            --Connect inferred DFFs LSB-to-MSB
                 END LOOP;
-                q_sig(bit_depth-1) <= '0';                       --LSB DFF gets a '1' input
-                encoder_q_o <= q_sig(1);                         --Assign encoder output w/ LSB DFF output
-                                                                 --Note that DATA is MSB first
-                                                                 --Also, first serial data bit is valid as soon as mux_select is
-                                                                 --asserted, not after it goes low since encoder_q_o gets encoder_d_i(bit_depth-1)
+                q_sig(bit_depth-1) <= '0';             --LSB DFF gets a '0' input for steady-state operation
+                encoder_q_o <= q_sig(1);               --Assign encoder output w/ MSB DFF output
+                                                       --Note that DATA is MSB first
+                                                       --Also, first serial data bit is valid first BCK_i rising edge after
+                                                       --edge_LRCK_sig is asserted
             END IF;
         END IF;
     END process encoder_process;
--- pcm_encoder_frame_clock_tb.vhd
+++ pcm_encoder_frame_clock_tb.vhd
@@ -1,18 +1,21 @@
 ------------------------------------------------------------------------------
 --Company:      Loyola Marymount University
---Engineer:     Joseph Gorman
+--Engineer:     Austin Pohlman
 
---Create Date:  04/7/2019
+--Create Date:  04/07/2019
 --Design Name:  pcm_encoder_tb.vhd
 --Module Name:
 --Project Name: PCM Communication System
 --Tool Versions:
 
 --Description:
-
+    --Test bench for pcm_encoder stage
 
 --Dependencies:
+    --pcm_encoder.vhd
 --Revisions:
+    --A (04/07/2019) : created file, instantiated pcm_encoder(behav)
+    --See github for full revision history
 ------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
@@ -26,36 +29,31 @@ entity pcm_encoder_tb is
 end entity;
 
 architecture test of pcm_encoder_tb is
-    constant bit_depth_tb  : integer := 8;
-    --signal b_clk_i_s       : std_logic;
+    constant bit_depth_tb  : integer := 8; --Needed for variable input data vectors
     signal L_encoder_d_i_s : std_logic_vector(bit_depth_tb-1 downto 0);
     signal R_encoder_d_i_s : std_logic_vector(bit_depth_tb-1 downto 0);
-    --signal LRCK_i_s        : std_logic;
     signal rst_i_async_s   : std_logic;
     signal MCLK_i_s        : std_logic;
     signal encoder_q_o_s   : std_logic;
 
     constant clock_period : time := 1 ns; --shortened from 35ns for simulation
-    --constant frame_clock_period : time := 560 ns; -- 35* 16 = 560
 
     begin
-        uut: entity work.pcm_encoder(behav)
+        uut: entity work.pcm_encoder(behav) --Change these generics to use variable bitdepth/samplefreq
         generic map(
             bit_depth => 8,
             sample_freq => 44100,
             MCLK_freq => 28224000 --24576000 used for 48k, 96k, 192k sampling rates
         )
         port map(
-            --b_clk_i       => b_clk_i_s,
             L_encoder_d_i => L_encoder_d_i_s,
             R_encoder_d_i => R_encoder_d_i_s,
-            --LRCK_i        => LRCK_i_s,
             rst_i_async   => rst_i_async_s,
             MCLK_i        => MCLK_i_s,
             encoder_q_o   => encoder_q_o_s
         );
 
-        bit_clock_process : process
+        bit_clock_process : process --Create MCLK_i
         begin
             MCLK_i_s <= '0';
             wait for clock_period/2;
@@ -63,20 +61,12 @@ architecture test of pcm_encoder_tb is
             wait for clock_period/2;
         end process;
 
-        --frame_clock_process : process
-        --begin
-        --    LRCK_i_s <= '0';
-        --    wait for frame_clock_period/2;
-        --    LRCK_i_s <= '1';
-        --    wait for frame_clock_period/2;
-        --end process;
-
         stim_process : process
         begin
               L_encoder_d_i_s <= "11001101"; --"1100110101100110" for 16-bit
               R_encoder_d_i_s <= "01110100"; --"0111010011110101"
               rst_i_async_s <= '1';
-              wait for 50 ns;
+              wait for 50 ns; --This is arbitrary
               rst_i_async_s <= '0';
               wait for 950 ns; --950ns to 2000ns depending on bit depth and sample freq
               L_encoder_d_i_s <= "11110000"; --"1111000011110000"
@@ -84,77 +74,5 @@ architecture test of pcm_encoder_tb is
               wait for 1000 ns;
               rst_i_async_s <= '1';
               wait;
-
-
-
-
---            rst_i_async_s <= '1';
---            mux_select_s <= '1';
---            encoder_d_i_s <= "00011100";
---            wait for 15 ns;
---            rst_i_async_s <= '0';
---            mux_select_s <= '1';
---            encoder_d_i_s <= "00011100";
---            wait for 10 ns;
---            rst_i_async_s <= '0';
---            mux_select_s <= '0';
---            encoder_d_i_s <= "00011100";
---            wait for 10 ns;
---            rst_i_async_s <= '0';
---            mux_select_s <= '0';
---            encoder_d_i_s <= "00011100";
---            wait for 10 ns;
---            rst_i_async_s <= '0';
---            mux_select_s <= '0';
---            encoder_d_i_s <= "00011100";
---            wait for 10 ns;
---            rst_i_async_s <= '0';
---            mux_select_s <= '0';
---            encoder_d_i_s <= "00011100";
---            wait for 10 ns;
---            rst_i_async_s <= '0';
---            mux_select_s <= '0';
---            encoder_d_i_s <= "00011100";
---            wait for 10 ns;
---            rst_i_async_s <= '0';
---            mux_select_s <= '0';
---            encoder_d_i_s <= "00011100";
---            wait for 10 ns;
---            rst_i_async_s <= '0';
---            mux_select_s <= '0';
---            encoder_d_i_s <= "00011100";
---            wait for 10 ns;
---            rst_i_async_s <= '0';
---            mux_select_s <= '1';
---            encoder_d_i_s <= "01110111";
---            wait for 10 ns;
---            rst_i_async_s <= '0';
---            mux_select_s <= '0';
---            encoder_d_i_s <= "01110111";
---            wait for 10 ns;
---            rst_i_async_s <= '0';
---            mux_select_s <= '0';
---            encoder_d_i_s <= "01110111";
---            wait for 10 ns;
---            rst_i_async_s <= '0';
---            mux_select_s <= '0';
---            encoder_d_i_s <= "01110111";
---            wait for 10 ns;
---            rst_i_async_s <= '0';
---            mux_select_s <= '0';
---            encoder_d_i_s <= "01110111";
---            wait for 10 ns;
---            rst_i_async_s <= '0';
---            mux_select_s <= '0';
---            encoder_d_i_s <= "01110111";
---            wait for 10 ns;
---            rst_i_async_s <= '0';
---            mux_select_s <= '0';
---            encoder_d_i_s <= "01110111";
---            wait for 10 ns;
---            rst_i_async_s <= '0';
---            mux_select_s <= '0';
---            encoder_d_i_s <= "01110111";
---            wait for 10 ns;
         end process;
 end test;
